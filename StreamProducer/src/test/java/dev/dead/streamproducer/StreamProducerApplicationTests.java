f8d85276b465ec95fe983f82d59f6cda1f27de04
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-02-08 14:21:49 +0300

    initialize Gradle project with Spring Boot and basic configurations

---
+++ StreamProducer/src/test/java/dev/dead/streamproducer/StreamProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dead.streamproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
